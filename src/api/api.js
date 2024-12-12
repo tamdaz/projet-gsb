b2310cc4a1b8b8954d1d03d11119234fba189ab7
tamdaz <tamda.zohir.pro@gmail.com>
2024-12-12 09:59:07 +0100

    Version 3.3.2 : Changement de l'@IP de l'API de GSB

--- src/api/api.js
+++ src/api/api.js
@@ -4,5 +4,5 @@ import axios from "axios";
  * Permet de se connecter à l'API de GSB.
  */
 export default axios.create({
-    baseURL: "http://172.16.61.61/restGSB"
+    baseURL: "http://127.0.0.1/restGSB"
 })
