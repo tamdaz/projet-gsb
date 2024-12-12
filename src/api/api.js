9d2f084266e6484c790630305e681fc7e00bedff
AyoubHsni <hassani.ayoub.scolaire@gmail.com>
2024-12-12 12:20:12 +0100

    gsb projet

--- src/api/api.js
+++ src/api/api.js
@@ -4,5 +4,5 @@ import axios from "axios";
  * Permet de se connecter à l'API de GSB.
  */
 export default axios.create({
-    baseURL: "http://127.0.0.1/restGSB"
+    baseURL: "http://172.16.61.6/restGSB"
 })
