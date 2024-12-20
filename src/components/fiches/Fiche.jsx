0fbbfa94224e4733cd826872b81fa9a315ed6cee
tamdaz <tamda.zohir.pro@gmail.com>
2024-12-20 10:03:33 +0100

    Version 5 : Amélioration de la page "Médecins". - Possibilité de voir & MAJ la fiche médecin. - Possibilité de voir les rapports.

---
+++ src/components/fiches/Fiche.jsx
@@ -0,0 +1,85 @@
+import React from "react";
+import Alert from "./../Alert";
+import { updateMedecin } from "../../api/medecins";
+
+/**
+ * Permet d'afficher un formulaire afin que l'utilisateur puisse mettre à
+ * jour les informations du médecin.
+ */
+export default function Fiche({ medecin }) {
+  const [status, setStatus] = React.useState(null);
+  const [message, setMessage] = React.useState(null);
+
+  /**
+   * Permet de mettre à jour les informations du médecin.
+   * @param {Event} e 
+   */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let data = Object.fromEntries(new FormData(e.target));
+    data.id = medecin.id;
+
+    updateMedecin(data).then(res => {
+      if (res.status === 200) {
+        setStatus("success");
+        setMessage(res.data.message);
+      } else {
+        setStatus("error");
+        setMessage("Une erreur s'est produite lors de la mise à jour de la fiche médécin.");
+      }
+    });
+  }
+
+  return <>
+    { status !== null && message !== null ? <Alert title={message} /> : null }
+    <form onSubmit={handleSubmit} method="post">
+      <div className="grid grid-cols-2 gap-4 mb-4">
+        <label>
+          Nom :
+          <input
+            type="text" name="nom"
+            defaultValue={medecin.nom}
+          />
+        </label>
+        <label>
+          Prénom :
+          <input
+            type="text" name="prenom"
+            defaultValue={medecin.prenom}
+          />
+        </label>
+        <label>
+          Département :
+          <input
+            type="text" name="departement"
+            defaultValue={medecin.departement}
+          />
+        </label>
+        <label>
+          Numéro de téléphone :
+          <input
+            type="tel" name="tel"
+            defaultValue={medecin.tel}
+          />
+        </label>
+        <label className="col-span-2">
+          Adresse :
+          <input
+            type="text" name="adresse"
+            defaultValue={medecin.adresse}
+          />
+        </label>
+      </div>
+      <label>
+        Spécialité complémentaire :
+        <input
+          type="text" name="specialite"
+          defaultValue={medecin.specialite_complementaire}
+        />
+      </label>
+      <br />
+      <button type="submit" className="w-full">Mettre à jour</button>
+    </form>
+  </>
+}
---
+++ src/components/fiches/Rapports.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+import { getRapports } from "../../api/rapports";
+
+/**
+ * Affiche des informations sur un médecin en question.
+ */
+export default function Rapports({ id }) {
+  const [rapports, setRapports] = React.useState([]);
+
+  React.useEffect(() => {
+    getRapports(id).then(res => {
+      console.log("Rapport", res);
+      setRapports(res.data);
+    })
+  }, []);
+
+  return <table className="border w-full">
+    <thead>
+      <tr>
+        <th className="border px-4 py-2 bg-blue-500 text-white">Date</th>
+        <th className="border px-4 py-2 bg-blue-500 text-white">Motif</th>
+        <th className="border px-4 py-2 bg-blue-500 text-white">Bilan</th>
+        <th className="border px-4 py-2 bg-blue-500 text-white">Visiteur</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        rapports.map((v, k) => {
+          return <tr key={`rapport-ligne-${k}`}>
+            <td className="border px-4 py-2">{ new Date(v.date).toLocaleDateString() }</td>
+            <td className="border px-4 py-2">{ v.motif }</td>
+            <td className="border px-4 py-2">{ v.bilan }</td>
+            <td className="border px-4 py-2">{ v.nom } { v.prenom }</td>
+          </tr>
+        })
+      }
+    </tbody>
+  </table>
+}
