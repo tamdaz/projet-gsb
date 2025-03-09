b174962b77bbb9e442cd59db988c8b39a2909f31
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-03-09 13:24:33 +0100

    Version 5.1.1 : MàJ documentation dans le fichier `api.js`

--- src/api/api.js
+++ src/api/api.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 /**
  * Permet de se connecter à l'API de GSB.
- * 
- * Bientôt, la variable de l'API d'URL sera mis dans le fichier .env
- * afin de la changer facilement.
  */
 export default axios.create({
     baseURL: import.meta.env.VITE_API_URL
