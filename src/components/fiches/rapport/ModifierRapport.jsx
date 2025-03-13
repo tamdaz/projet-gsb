2c1119063b84d51440859a774c8144c9b4258592
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-03-13 20:22:27 +0100

    Version 5.6 : Amélioration d'une fonctionnalité permettant de MàJ un rapport.

--- src/components/fiches/rapport/ModifierRapport.jsx
+++ src/components/fiches/rapport/ModifierRapport.jsx
@@ -1,14 +1,15 @@
 import React from "react";
-import { useOutletContext } from "react-router-dom";
-import { getRapportsParDate, getRapportsParId } from "../../../api/rapports";
 import Alert from "./../../../components/Alert";
+import { useOutletContext } from "react-router-dom";
+import { getRapportsParDate, modifierRapport } from "../../../api/rapports";
 
 export default function ModifierRapport() {
     const [status, setStatus] = React.useState(null);
     const [message, setMessage] = React.useState(null);
     const [dateRapport, setDateRapport] = React.useState(null);
-    const [dataVisiteur, medecinTrouvee] = useOutletContext();
     const [listeRapports, setListeRapports] = React.useState([]);
+    const [selectedRapport, setSelectedRapport] = React.useState({});
+    const [dataVisiteur] = useOutletContext();
 
     React.useEffect(() => {
         if (dateRapport !== undefined) {
@@ -47,6 +48,13 @@ export default function ModifierRapport() {
      * Permet d'afficher un ensemble de rapports.
      */
     const TableauRapports = () => {
+        const isRapportsEmpty = listeRapports.length === 0;
+        const isRapportSelected = Object.keys(selectedRapport).length !== 0;
+
+        if (isRapportsEmpty || isRapportSelected) {
+            return null;
+        }
+
         return <table className="border w-full">
             <thead>
                 <tr>
@@ -58,8 +66,8 @@ export default function ModifierRapport() {
             </thead>
             <tbody>
                 {
-                    listeRapports.map((rapport, k) => {
-                        return <tr key={`rapport-ligne-${k}`}>
+                    listeRapports != [] && listeRapports.map((rapport, k) => {
+                        return <tr onClick={() => setSelectedRapport(rapport)} className="hover:bg-neutral-100 active:bg-neutral-200 cursor-pointer" key={`rapport-ligne-${k}`}>
                             <td className="border px-4 py-2">{rapport.idRapport}</td>
                             <td className="border px-4 py-2">{rapport.motif}</td>
                             <td className="border px-4 py-2">{rapport.bilan}</td>
@@ -71,15 +79,60 @@ export default function ModifierRapport() {
         </table>
     }
 
+    /**
+     * Permet de mettre à jour un rapport une fois le formulaire soumis.
+     * @param {Event} e 
+     */
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        let data = Object.fromEntries(new FormData(e.target));
+        data.idRapport = selectedRapport.idRapport;
+
+        modifierRapport(data).then(res => {
+            if (res.status === 200) {
+                setStatus("success");
+                setMessage("Rapport mis à jour avec succès.");
+            } else {
+                setStatus("error");
+                setMessage("Une erreur s'est produite lors de la modification de ce rapport.");
+            }
+        });
+    }
+
+    /**
+     * Permet d'afficher le formulaire.
+     */
+    const FormulaireRapport = () => {
+        if (Object.keys(selectedRapport).length === 0) {
+            return null;
+        }
+
+        return <form onSubmit={handleSubmit}>
+            <label>
+                Motif :
+                <input type="text" name="motif" defaultValue={selectedRapport.motif} required />
+            </label>
+            <label>
+                Bilan :
+                <textarea name="bilan" defaultValue={selectedRapport.bilan} required></textarea>
+            </label>
+            <button type="submit" className="w-full">Ajouter un rapport</button>
+        </form>
+    }
+
     return <>
         {status !== null && message !== null ? <Alert title={message} /> : null}
-        <label>
-            Choisir la date :
-            <div className="flex">
-                <input type="date" onChange={(e) => setDateRapport(e.target.value)} value={dateRapport} />
-            </div>
-        </label>
-        <br />
-        { listeRapports.length !== 0 && <TableauRapports /> }
+        {
+            Object.keys(selectedRapport).length === 0 && <label>
+                Choisir la date :
+                <div className="flex">
+                    <input type="date" onChange={(e) => setDateRapport(e.target.value)} value={dateRapport} />
+                </div>
+                <br />
+            </label>
+        }
+        <TableauRapports />
+        <FormulaireRapport />
     </>
 }
