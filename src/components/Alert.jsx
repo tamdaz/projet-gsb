54f77edecca453c2888394471df603282073a751
Zohir Tamda <tamda.zohir.pro@gmail.com>
2025-03-14 12:08:51 +0100

    Version 5.7 : Amélioration du composant Alert.

--- src/components/Alert.jsx
+++ src/components/Alert.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 /**
  * Permet d'afficher une alerte. Ce composant a pour but
  * d'attirer l'attention à l'utilisateur.
@@ -7,8 +5,24 @@ import React from 'react'
  * @param {{ title: string }} props
  * @returns 
  */
-export default function Alert({ title }) {
-	return <div className="w-full px-4 py-2 mb-4 border rounded-md bg-gray-100 border-gray-300">
+export default function Alert({ title, status }) {
+	/**
+	 * Récupérer les propriétés de Tailwind pour styliser l'alerte.
+	 * 
+	 * @param {"success"|"error"|"unknown"} name 
+	 * @returns 
+	 */
+	const getColors = (status="unknown") => {
+		const colors = {
+			success: "bg-green-100 border-green-300",
+			error: "bg-red-100 border-red-300",
+			unknown: "bg-gray-100 border-gray-300",
+		}
+
+		return colors[status];
+	}
+
+	return <div className={`w-full px-4 py-2 mb-4 border rounded-md ${getColors(status)}`}>
 		<h4 className="text-xl font-bold">{title}</h4>
 	</div>
 }
